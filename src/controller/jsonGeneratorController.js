82b55f3193b00f7ae8d46f2c0c864ce052bdf222
felipeimp22 <felipeimperio.imperioo@gmail.com>
2020-10-18 18:48:25 -0300

    Cleaning logs and change variable names

--- src/controller/jsonGeneratorController.js
+++ src/controller/jsonGeneratorController.js
@@ -1,26 +1,52 @@
 class JsonGenerator{
+
     healthCheck(req,res){
         return res.json('Live')
     }
+
+
    async generateJson(req,res){
     const {data, field } = req.body
     try{
     let randomArr = []
     data.forEach(e => randomArr.push(e))
-console.log(data)
+    console.log(data)
     const chooseAValue = Math.floor(Math.random() * randomArr.length)
     
-//    const t =  JSON.stringify(`'{ "${field}" : "${randomArr[chooseAValue]}"}'`)
-        const t = '{"'+field+'":"'+randomArr[chooseAValue]+'"}'
-   const t2 = JSON.parse(t)
+        const stringFyFields = '{"'+field+'":"'+randomArr[chooseAValue]+'"}'
+   const parseJson = JSON.parse(stringFyFields)
   
   
-   console.log(t2)
-    return res.json(t2)
+   console.log(parseJson)
+    return res.json(parseJson)
     }catch(err){
         console.log(err)
     }
     }
+
+
+
+
+
+    async dynamicJson(req,res){
+        const {data, field } = req.body
+        try{
+        let randomArr = []
+        data.forEach(e => randomArr.push(e))
+        console.log(data)
+        const chooseAValue = Math.floor(Math.random() * randomArr.length)
+        
+    //    const t =  JSON.stringify(`'{ "${field}" : "${randomArr[chooseAValue]}"}'`)
+            const t = '{"'+field+'":"'+randomArr[chooseAValue]+'"}'
+       const t2 = JSON.parse(t)
+      
+      
+       console.log(t2)
+        return res.json(t2)
+        }catch(err){
+            console.log(err)
+        }
+        }
 
 
 
